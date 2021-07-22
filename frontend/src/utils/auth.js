0ea6f87afc2543f859b82eb8994a83b9a8b48b27
Assel Kadyrkul <asel11k97@gmail.com>
2021-07-22 23:27:54 +0100

    fix: remove extra slash from base ur

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.assel.students.nomoreparties.site/';
+export const BASE_URL = 'https://api.assel.students.nomoreparties.site';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
